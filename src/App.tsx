0614256aeb29b65aa97b554c533984822988d083
joddern <kristtjo@stud.ntnu.no>
2023-08-08 14:18:55 +0200

    Made peg solitaire render with clicking

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import "./App.css";
+import Game from "./components/GameBoard";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <p>Hello!</p>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
 }
